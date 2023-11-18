bf3436fa243c03c487c1ca3a09ea60e58f0d2248
Ashutosh Swamy <76511506+ashutoshswamy@users.noreply.github.com>
2023-11-19 00:01:36 +0530

    Add files via upload

---
+++ Python/simple_graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [2, 4, 6,8, 10]
+
+plt.plot(x_values, y_values)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Graph")
+
+plt.show()
